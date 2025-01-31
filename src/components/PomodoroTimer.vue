4fc2c74c2ee3b67490cee136798d156edbf24e44
celina-ngth <ngeth.celina@gmail.com>
2025-01-31 13:49:19 +0100

    feat: make buttons reactive

--- src/components/PomodoroTimer.vue
+++ src/components/PomodoroTimer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-	import { computed, ref, watch } from 'vue'
+	import { computed, reactive, ref, watch } from 'vue'
 
 	const TIMERS: Record<string, number> = {
 		focus: 0.1 * 60,
@@ -8,17 +8,16 @@
 	}
 
 	const isRunning = ref(false)
-	let currentTimer = ref('focus')
-	const timer = ref(TIMERS[currentTimer.value])
+	const currentTimer = ref('focus')
+	const timer = reactive({
+		value: TIMERS[currentTimer.value],
+	})
+	const intervalId = ref<number | null>(null)
 
 	const formattedTime = computed(() => {
 		const minutes = Math.floor(timer.value / 60)
 		const seconds = timer.value % 60
 		return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`
-	})
-
-	watch(timer, () => {
-		formattedTime.value
 	})
 
 	const startPauseTimer = () => {
@@ -52,15 +51,17 @@
 		}
 	}
 
-	const intervalId = ref<number | null>(null)
+	watch(currentTimer, () => {
+		timer.value = TIMERS[currentTimer.value]
+	})
 </script>
 
 <template>
 	<h1>{{ currentTimer }}</h1>
 
-	<button type="button" @click="timer = TIMERS['focus']">Focus</button>
-	<button type="button" @click="timer = TIMERS['break']">Break</button>
-	<button type="button" @click="timer = TIMERS['longBreak']">Long break</button>
+	<button type="button" @click="currentTimer = 'focus'">Focus</button>
+	<button type="button" @click="currentTimer = 'break'">Break</button>
+	<button type="button" @click="currentTimer = 'longBreak'">Long break</button>
 
 	<div class="card">
 		<p>{{ formattedTime }}</p>
