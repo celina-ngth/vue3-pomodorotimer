060c08dce9756117ddd599af5430e09b67f57f6c
celina-ngth <ngeth.celina@gmail.com>
2025-01-31 13:57:15 +0100

    feat: change displayed labels

--- src/components/PomodoroTimer.vue
+++ src/components/PomodoroTimer.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
 	import { computed, reactive, ref, watch } from 'vue'
 
+	const TIMERS_LABEL: Record<string, string> = {
+		focus: 'Focus',
+		break: 'Break',
+		longBreak: 'Long break',
+	}
+
 	const TIMERS: Record<string, number> = {
 		focus: 0.1 * 60,
 		break: 0.05 * 60,
 		longBreak: 0.07 * 60,
 	}
 
-	const isRunning = ref(false)
-	const currentTimer = ref('focus')
+	const isRunning = ref<boolean>(false)
+	const currentTimer = ref<string>('focus')
+	const intervalId = ref<number | null>(null)
 	const timer = reactive({
 		value: TIMERS[currentTimer.value],
 	})
-	const intervalId = ref<number | null>(null)
 
 	const formattedTime = computed(() => {
 		const minutes = Math.floor(timer.value / 60)
@@ -30,12 +36,9 @@
 				timer.value -= 1
 
 				if (timer.value <= 0) {
-					clearInterval(intervalId.value ?? 0)
 					isRunning.value = false
-
+					clearInterval(intervalId.value ?? 0)
 					changeTimer()
-
-					timer.value = TIMERS[currentTimer.value]
 				}
 			}, 1000)
 		}
@@ -61,11 +64,17 @@
 </script>
 
 <template>
-	<h1>{{ currentTimer }}</h1>
+	<h1>{{ TIMERS_LABEL[currentTimer] }}</h1>
 
-	<button type="button" @click="currentTimer = 'focus'">Focus</button>
-	<button type="button" @click="currentTimer = 'break'">Break</button>
-	<button type="button" @click="currentTimer = 'longBreak'">Long break</button>
+	<button type="button" @click="currentTimer = 'focus'">
+		{{ TIMERS_LABEL['focus'] }}
+	</button>
+	<button type="button" @click="currentTimer = 'break'">
+		{{ TIMERS_LABEL['break'] }}
+	</button>
+	<button type="button" @click="currentTimer = 'longBreak'">
+		{{ TIMERS_LABEL['longBreak'] }}
+	</button>
 
 	<div class="card">
 		<p>{{ formattedTime }}</p>
